47eb088e1c0fd0a2433921aa0b77548b5129e325
Crowdbotics <team@crowdbotics.com>
2021-09-08 21:23:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "super_wind_30400Identifier",
+  url: "https://super_wind_30400Identifier.botics.co",
+  api: "https://super_wind_30400Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
